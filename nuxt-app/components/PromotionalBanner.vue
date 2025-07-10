b4726c360bbf174ee03725c514600730c9ea1a1c
PedroBzra <pedro.fernandobzra@gmail.com>
2025-07-10 20:31:26 -0300

    feat: add PromotionalBanner component to the homepage

---
+++ nuxt-app/components/PromotionalBanner.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+</script>
+<template>
+  <button class="bg-[#142F3F] w-full h-20 flex flex-row">
+    <div class="w-2/3 flex flex-col text-center justify-center p-4">
+        <h5 class="text-white font-extrabold">NOVIDADE EM IPANEMA</h5>
+        <h2 class="text-[#C0730F] font-extrabold text-2xl">ALMA IPANEMA</h2>
+    </div>
+    <div class="w-1/3 overflow-hidden"> 
+      <nuxt-img 
+          src="/img/exemploG.jpg" 
+          class="w-full h-full object-cover"
+          alt="Imagem do Empreendimento"
+      />
+    </div>
+  </button>
+</template>
--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="min-h-screen">
     <AppHeader />
-    <main class="p-4 space-y-8 gap-5 flex flex-col">
-      <SearchBar />
-      <HeroCarousel />
-      <MapCTA />
-      <FeaturedPropertiesCard />
-      <RegionNavigation />
+    <main class="space-y-8 gap-5 flex flex-col">
+      <SearchBar class="p-4"/>
+      <PromotionalBanner />
+      <HeroCarousel class="p-4"/>
+      <MapCTA class="p-4"/>
+      <FeaturedPropertiesCard class="p-4"/>
+      <RegionNavigation class="p-4"/>
     </main>
   </div>
 </template>
