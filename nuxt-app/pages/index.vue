4088722ca1c418e199c3d2304c1d89909e0cffba
PedroBzra <pedro.fernandobzra@gmail.com>
2025-07-10 21:55:36 -0300

    refactor: adjust padding and spacing for consistency in index page layout

--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="min-h-screen">
     <AppHeader />
-    <main class="space-y-8 gap-5 flex flex-col">
-      <SearchBar class="p-4" />
-      <PromotionalBanner />
-      <HeroCarousel class="p-4" />
-      <MapCTA class="p-4" />
-      <FeaturedPropertiesCarousel class="p-4" />
-      <RegionNavigation class="p-4" />
+    <main class="p-4 space-y-8 flex flex-col">
+      <SearchBar/>
+      <PromotionalBanner/>
+      <HeroCarousel/>
+      <MapCTA/>
+      <FeaturedPropertiesCarousel />
+      <RegionNavigation/>
     </main>
     <AppFooter />
   </div>
