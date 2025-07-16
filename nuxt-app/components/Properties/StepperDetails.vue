d676ad7591e887e9de6babf97769876ea6a31db5
LuanBz <lbezerra@sga.pucminas.br>
2025-07-16 00:47:17 -0300

    feat: add StepperDetails component to property details page for enhanced status display

---
+++ nuxt-app/components/Properties/StepperDetails.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import type { StepperItem } from "@nuxt/ui";
+
+const items = ref<StepperItem[]>([
+  {
+    title: "Pré-lançamento",
+    description: "Antecipe seu interesse",
+    icon: "mdi:rocket-launch",
+  },
+  {
+    title: "Lançamento",
+    description: "Hora de escolher seu imóvel",
+    icon: "mdi:play",
+  },
+  {
+    title: "Em Obras",
+    description: "Entrega prevista para 2024",
+    icon: "mdi:tools",
+  },
+  {
+    title: "Pronto",
+    description: "Seu novo lar está pronto",
+    icon: "i-lucide-home",
+  },
+]);
+</script>
+
+<template>
+  <UStepper :items="items" size="lg" orientation="vertical" class="w-full" />
+</template>
--- nuxt-app/pages/properties/[id].vue
+++ nuxt-app/pages/properties/[id].vue
@@ -1,30 +1,37 @@
 <script setup>
-import { ref } from 'vue'
-const route = useRoute()
-const id = route.params.id
+import { ref } from "vue";
+import StepperDetails from "~/components/Properties/StepperDetails.vue";
+const route = useRoute();
+const id = route.params.id;
 
 // Simulando a propriedade (você pode substituir por fetch ou useAsyncData)
 const property = ref({
   id,
-  title: 'Almar Ipanema',
-  district: 'Barra da Tijuca',
-  price: 1214111.20,
+  title: "Almar Ipanema",
+  district: "Barra da Tijuca",
+  price: 1214111.0,
+  m2: 14200,
   location: { lat: -23.55052, lng: -46.633308 },
   features: {
     bedrooms: 2,
     bathrooms: 2,
     area: 85,
-    garage: 1
+    garage: 1,
+    rooms: 1,
+    suites: 1,
+    floor: 5,
   },
-  type: 'Residencial',
-  status: 'Em construção',
-  delivery: '2024',
-  description: 'Apartamento com vista para o mar, área de lazer completa e acabamento de alto padrão.'
-})
+  type: "Residencial",
+  status: "Em construção",
+  delivery: "2024",
+  description:
+    "Apartamento com vista para o mar, área de lazer completa e acabamento de alto padrão.",
+});
 
-const mapSrc = computed(() =>
-  `https://www.google.com/maps/embed/v1/view?key=AIzaSyAj4WoCauiEdeCiihMBHl8rYFLjYmI7CMk&center=${property.value.location.lat},${property.value.location.lng}&zoom=18&maptype=roadmap`
-)
+const mapSrc = computed(
+  () =>
+    `https://www.google.com/maps/embed/v1/view?key=AIzaSyAj4WoCauiEdeCiihMBHl8rYFLjYmI7CMk&center=${property.value.location.lat},${property.value.location.lng}&zoom=18&maptype=roadmap`
+);
 </script>
 
 <template>
@@ -33,37 +40,53 @@ const mapSrc = computed(() =>
     <PromotionalBanner />
     <PropertiesImageCarousel />
 
-    <div class="flex-col">
-      <p class="font-bold">{{ property.district }}</p>
-      <h2 class="font-extrabold text-[#C0730F] text-3xl uppercase">{{ property.title }}</h2>
+    <div class="flex flex-row gap-4">
+      <div class="flex-col flex gap-4">
+        <div>
+          <p class="font-bold">{{ property.district }}</p>
+          <h2 class="font-extrabold text-[#C0730F] text-3xl uppercase">
+            {{ property.title }}
+          </h2>
+        </div>
 
-      <div class="flex flex-row gap-2 mt-4">
-        <div class="bg-[#F0F0F0] dark:bg-[#141F3F] p-2 rounded-lg flex justify-center gap-2">
-          <UIcon name="mdi:bed" class="size-6" />
-          <p class="font-extrabold text-[#141F3F] dark:text-[#F0F0F0]">{{ property.features.bedrooms }}</p>
-        </div>
-        <div class="bg-[#F0F0F0] dark:bg-[#141F3F] p-2 rounded-lg flex justify-center gap-2">
-          <UIcon name="mdi:shower-head" class="size-6" />
-          <p class="font-extrabold text-[#141F3F] dark:text-[#F0F0F0]">{{ property.features.bathrooms }}</p>
-        </div>
-        <div class="bg-[#F0F0F0] dark:bg-[#141F3F] p-2 rounded-lg flex justify-center gap-2">
-          <UIcon name="mdi:ruler-square" class="size-6" />
-          <p class="font-extrabold text-[#141F3F] dark:text-[#F0F0F0]">{{ property.features.area }} m²</p>
-        </div>
-        <div class="bg-[#F0F0F0] dark:bg-[#141F3F] p-2 rounded-lg flex justify-center gap-2">
-          <UIcon name="mdi:garage" class="size-6" />
-          <p class="font-extrabold text-[#141F3F] dark:text-[#F0F0F0]">{{ property.features.garage }}</p>
+        <div class="flex flex-col">
+          <div class="flex gap-2">
+            <UBadge color="success">Abaixo do mercado</UBadge
+            ><UBadge>Oportunidade</UBadge>
+          </div>
+          <h2
+            class="font-extrabold text-[#141F3F] dark:text-[#F0F0F0] text-3xl uppercase"
+          >
+            R$
+            {{
+              property.price.toLocaleString("pt-BR", {
+                minimumFractionDigits: 2,
+              })
+            }}
+          </h2>
+          <h3>
+            Preço por m² :
+            {{
+              property.m2.toLocaleString("pt-BR", { minimumFractionDigits: 2 })
+            }}
+          </h3>
         </div>
       </div>
-
-      <h2 class="font-extrabold text-[#141F3F] dark:text-[#F0F0F0] text-3xl uppercase">
-        R$ {{ property.price.toLocaleString('pt-BR', { minimumFractionDigits: 2 }) }}
-      </h2>
     </div>
-
-    <UButton variant="soft" class="justify-center bg-[#C0730F] text-white w-full h-10 rounded-lg">
-      Marque uma visita!
-    </UButton>
+    <div class="flex flex-col gap-2">
+      <UButton
+        variant="soft"
+        class="justify-center bg-[#C0730F] text-white w-full h-15 rounded-lg"
+      >
+        Marque uma visita!
+      </UButton>
+      <UButton
+        variant="soft"
+        class="justify-center bg-[#141F3F] text-white w-full h-15 rounded-lg"
+      >
+        Peça mais informações
+      </UButton>
+    </div>
 
     <iframe
       height="250"
@@ -74,19 +97,111 @@ const mapSrc = computed(() =>
       referrerpolicy="no-referrer-when-downgrade"
     ></iframe>
 
-    <h2 class="font-extrabold text-[#C0730F] text-3xl uppercase">Ficha Técnica</h2>
+    <div class="flex flex-col gap-4">
+      <h2 class="font-extrabold text-[#C0730F] text-3xl uppercase">Status</h2>
+      <StepperDetails />
+    </div>
+
+    <h2 class="font-extrabold text-[#C0730F] text-3xl uppercase">
+      Ficha Técnica
+    </h2>
     <div class="flex flex-col gap-2">
       <div class="flex flex-row gap-2">
-        <p class="font-bold">Tipo:</p>
-        <p>{{ property.type }}</p>
+        <UCard variant="solid" class="bg-[#F0F0F0] dark:bg-[#141F3F] w-1/3">
+          <div class="flex flex-col items-center justify-center gap-2">
+            <UIcon
+              name="mdi-home"
+              class="text-[#141F3F] dark:text-[#F0F0F0] size-12"
+            />
+            <p class="font-boldtext-[#141F3F] dark:text-[#F0F0F0]">
+              {{ property.type }}
+            </p>
+          </div>
+        </UCard>
+        <UCard variant="solid" class="bg-[#F0F0F0] dark:bg-[#141F3F] w-1/3">
+          <div class="flex flex-col items-center justify-center gap-2">
+            <UIcon
+              name="mdi-calendar"
+              class="text-[#141F3F] dark:text-[#F0F0F0] size-12"
+            />
+            <p class="font-bold text-[#141F3F] dark:text-[#F0F0F0]">
+              {{ property.delivery }}
+            </p>
+          </div>
+        </UCard>
+        <UCard variant="solid" class="bg-[#F0F0F0] dark:bg-[#141F3F] w-1/3">
+          <div class="flex flex-col items-center justify-center gap-2">
+            <UIcon
+              name="mdi:ruler-square"
+              class="text-[#141F3F] dark:text-[#F0F0F0] size-12"
+            />
+            <p class="font-boldtext-[#141F3F] dark:text-[#F0F0F0]">
+              {{ property.features.area }}m²
+            </p>
+          </div>
+        </UCard>
       </div>
-      <div class="flex flex-row gap-2">
-        <p class="font-bold">Status:</p>
-        <p>{{ property.status }}</p>
+      <div
+        class="bg-[#F0F0F0] dark:bg-[#141F3F] p-4 rounded-lg flex gap-y-4 flex-wrap justify-center items-center"
+      >
+        <div class="flex justify-center gap-2 w-1/3">
+          <UIcon name="mdi:bed-empty" class="size-6 text-[#c0730f]" />
+          <p class="font-medium text-[#141F3F] dark:text-[#F0F0F0]">
+            {{ property.features.bedrooms }} Quartos
+          </p>
+        </div>
+        <div class="flex justify-center gap-2 w-1/3">
+          <UIcon name="mdi:shower-head" class="text-[#c0730f] size-6" />
+          <p class="font-medium text-[#141F3F] dark:text-[#F0F0F0]">
+            {{ property.features.bathrooms }} Banheiros
+          </p>
+        </div>
+        <div class="flex justify-center gap-2 w-1/3">
+          <UIcon name="mdi:garage" class="text-[#c0730f] size-6" />
+          <p class="font-medium text-[#141F3F] dark:text-[#F0F0F0]">
+            {{ property.features.garage }} Vagas
+          </p>
+        </div>
+        <div class="flex justify-center gap-2 w-1/3">
+          <UIcon name="mdi:bed" class="size-6 text-[#c0730f]" />
+          <p class="font-medium text-[#141F3F] dark:text-[#F0F0F0]">
+            {{ property.features.suites }} Suítes
+          </p>
+        </div>
+        <div class="flex justify-center gap-2 w-1/3">
+          <UIcon name="mdi:tv" class="size-6 text-[#c0730f]" />
+          <p class="font-medium text-[#141F3F] dark:text-[#F0F0F0]">
+            {{ property.features.rooms }} Sala
+          </p>
+        </div>
+        <div class="flex justify-center gap-2 w-1/3">
+          <UIcon name="mdi:office-building" class="size-6 text-[#c0730f]" />
+          <p class="font-medium text-[#141F3F] dark:text-[#F0F0F0]">
+            {{ property.features.floor }}º Andar
+          </p>
+        </div>
       </div>
-      <div class="flex flex-row gap-2">
-        <p class="font-bold">Entrega:</p>
-        <p>{{ property.delivery }}</p>
+      <h3 class="font-extrabold text-[#C0730F] text-lg uppercase mt-4">
+        Características
+      </h3>
+      <div class="dark:bg-[#141F3F] rounded-lg">
+        <ul class="list-disc list-inside p-4 gap-2 flex flex-col">
+          <li class="font-medium text-[#141F3F] dark:text-[#F0F0F0]">
+            Vista para o mar
+          </li>
+          <li class="font-medium text-[#141F3F] dark:text-[#F0F0F0]">
+            Área de lazer completa
+          </li>
+          <li class="font-medium text-[#141F3F] dark:text-[#F0F0F0]">
+            Acabamento de alto padrão
+          </li>
+          <li class="font-medium text-[#141F3F] dark:text-[#F0F0F0]">
+            Próximo a escolas e comércio
+          </li>
+          <li class="font-medium text-[#141F3F] dark:text-[#F0F0F0]">
+            Segurança 24h
+          </li>
+        </ul>
       </div>
     </div>
 
